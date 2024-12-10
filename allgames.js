ad58e29e364596feed2f987fb34afddab0e92201
Ravi Bhatt <rnbhatt@umich.edu>
2024-12-10 18:13:00 -0500

    Json system

---
+++ allgames.js
@@ -0,0 +1,15 @@
+const allgames = 
+{
+    "WBomb": {
+        title: "WBomb",
+        img: "wbomb/thumbnail.jpg",
+        year: 2002,
+        "desc": "A Bomberman Clone"
+    },
+    "dummygame": {
+        title: "Dummy Game",
+        img: "dummygame/thumb.jpg",
+        year: 2077,
+        "desc": "optional description delete this if you don't have a description"
+    },
+}
