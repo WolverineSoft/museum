f1bbbeb87733ea44bc92c34a75b769d98cf89961
Ravi Bhatt <rnbhatt@umich.edu>
2024-12-10 18:28:26 -0500

    Add roots

--- allgames.js
+++ allgames.js
@@ -4,18 +4,24 @@ const allgames =
         title: "WBomb",
         img: "WBomb/thumbnail.jpg",
         year: 2002,
-        "desc": "A Bomberman Clone"
+        desc: "A Bomberman Clone"
     },
     "zenimaze": {
         title: "zenimaze",
         img: "zenimaze/thumbnail.jpg",
         year: 2006,
-        "desc": "Explore a maze and collect pickups. Made with ZeniLib."
+        desc: "Explore a maze and collect pickups. Made with ZeniLib."
+    },
+    "https://rootsseries.github.io/":{
+        title: "Roots: The Series",
+        img: "https://rootsseries.github.io/2/thumb.png",
+        year: "2023-2024",
+        desc: "The entire Roots series! You aren't ready for the lore."
     },
     "dummygame": {
         title: "Dummy Game",
         img: "dummygame/thumb.jpg",
         year: 2077,
-        "desc": "optional description delete this if you don't have a description"
+        desc: "optional description delete this if you don't have a description"
     },
 }
