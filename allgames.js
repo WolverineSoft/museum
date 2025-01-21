4749c473abc4be7723903e6cc7e959880f9a1d2b
rnbhatt <55766810+rnbhatt@users.noreply.github.com>
2025-01-20 16:55:42 -0800

    sandman et al

--- allgames.js
+++ allgames.js
@@ -168,6 +168,12 @@ const allgames =
         year: 2005,
         desc: "Keep the dream inflated for as long as possible"
     },
+      "sandman": {
+        title: "he Sand Man defends Sand Land from the Monkey Hoard of Doom",
+        img: "sandman/thumbnail.jpg",
+        year: 2005,
+        desc: "The title tells you all you need to know"
+    },
      "perilous-seas": {
         title: "Perilous Seas",
         img: "perilous-seas/thumbnail.png",
